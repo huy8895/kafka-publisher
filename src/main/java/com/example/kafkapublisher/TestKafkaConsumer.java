b26935df7c2ead1512f9d263ed4c4e8c4b7433ec
huytvdev <huy8895@gmail.com>
2023-12-21 21:27:47 +0700

    done method get message from partition.

---
+++ src/main/java/com/example/kafkapublisher/TestKafkaConsumer.java
@@ -0,0 +1,73 @@
+package com.example.kafkapublisher;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicReference;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class TestKafkaConsumer {
+
+  public static final String DEFAULT_TOPIC = "test.topic.default";
+  @Value("${kafka.bootstrap-servers}")
+  private String BOOTSTRAP_SERVERS;
+
+  public Consumer<String, String> consumer() {
+    Properties consumerProperties = new Properties();
+    consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+    consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+        StringDeserializer.class.getName());
+    consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+        StringDeserializer.class.getName());
+    consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "ConsumerGroup1");
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProperties);
+    return consumer;
+  }
+
+  @GetMapping("/get-message-from-offset")
+  public String getMessageFromOffset(@RequestParam String topic,
+      @RequestParam int partition,
+      @RequestParam long offset) {
+    log.info("getMessageFromOffset() called with: [{}], [{}], [{}]", topic, partition, offset);
+    Consumer<String, String> consumer = consumer();
+
+    AtomicReference<ConsumerRecord<String, String>> record = new AtomicReference<>();
+    TopicPartition topicPartition = new TopicPartition(topic, partition);
+    List<TopicPartition> partitions = new ArrayList<>();
+    partitions.add(topicPartition);
+    consumer.assign(partitions);
+    consumer.assign(Collections.singletonList(topicPartition));
+    consumer.seek(topicPartition, offset);
+    ConsumerRecords<String, String> messages = consumer.poll(Duration.ofMillis(1000));
+
+    for (ConsumerRecord<String, String> message : messages) {
+      record.set(message);
+      break;
+    }
+    consumer.close();
+    record.get().timestamp();
+    log.info("record: {}", record);
+    return record.get().value();
+  }
+}
